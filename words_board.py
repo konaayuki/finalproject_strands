865f0605811c0786176ba8a061107a5209eafcde
Brooke Manning <brookemanning@brookes-air-7.devices.brown.edu>
2024-04-30 19:42:31 -0400

    Update words_board.py

--- words_board.py
+++ words_board.py
@@ -5,7 +5,7 @@ graph_board = np.zeros((8,6))
 
 #DEFINE BOARD
 board = [8,6]
-board = [[' ' for i in range(6)] for j in range(8)]
+board = [[ '_' for i in range(6)] for j in range(8)]
 
 #TESTING LIST
 world_piece = ["MARSH", "MOUNTAIN", "SWAMP", "TERRAIN", "PLAIN", "FOREST", "TUNDRA", "DESERT"]
@@ -13,36 +13,35 @@ world_piece = ["MARSH", "MOUNTAIN", "SWAMP", "TERRAIN", "PLAIN", "FOREST", "TUND
 import random
 
 def words_board (world_piece, graph_board, board):
-    for word in world_piece: #run for the number of words in the list
-        for letter in word:
-        
-            #randomly choose starting position
-            start_row = random.randint(0, 7) 
-            start_col = random.randint(0,5)
-            start_placement = (start_row,start_col)
-            print(start_placement)
-            current = letter
-            
-            if graph_board[start_placement[0]][start_placement[1]] == 0: #if the starting position is empty
-                print ('here')
-                possible_list = look_neighbours(graph_board, start_row, start_col)
-                select_options(graph_board, board,current, possible_list)
+    start_row = random.randint(0, 7) 
+    start_col = random.randint(0,5)
+    start_placement=[start_row,start_col]
+    print(start_placement)
 
-def select_options( graph_board, board,current, possible_list): #out of the empty spots found in look_neighbours, randomly select one spot
-     if possible_list:
+    for i in range (0, len(world_piece)):
+        word = world_piece[i]
+
+        for letter in word:
+            if graph_board[start_placement[0]][start_placement[1]] == 0: #if the starting position is empty
+                possible_list = look_neighbours(graph_board, start_row, start_col)
+                board,select_position = select_options(graph_board, board,letter, possible_list)
+                if select_position != start_placement:
+                    start_row = select_position[0]
+                    start_col=select_position[1]
+
+def select_options( graph_board, board,letter, possible_list): #out of the empty spots found in look_neighbours, randomly select one spot
+    if possible_list:
+        print ('here')
         select_position = random.choice(possible_list)
         x,y = select_position
         graph_board[x][y] = 1
-        board[x][y] = current
+        board[x][y] = letter
 
-    #  for index in n:
-    #      if graph_board(index) == 0:
-    #          graph_board()
+    return board, select_position
 
 def look_neighbours(graph_board, start_row, start_col): #function to check spots around
     possible_list = [] #create a list of possible options to go next
 
-    print ('here1')
     if (start_row + 1) < 8 and graph_board[start_row + 1][start_col] == 0: #checks option in the row above but the same column
         possible_list.append([start_row + 1, start_col])
 
