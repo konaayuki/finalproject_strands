975d31c74c017d45559cebc069d237d6cadbbb33
miareiland <144496029+miareiland@users.noreply.github.com>
2024-05-09 22:03:57 -0400

    "main loop and hint"

--- dictionaryapi.py
+++ dictionaryapi.py
@@ -1,5 +1,4 @@
-#import os
-pip install requests
+import os
 import random
 import requests
 #import urllib3
@@ -8,29 +7,46 @@ import requests
 #accessing datamuse api
 #searches for:
 #words that have related meaning to query word/phrase
-#max 50 words per query
-#limited to words of 4 to 12 letters
+#max 20 words per query
+#limited to words > 3 letters
 def datamuse_api_get(query):
-    url = 'https://api.datamuse.com/words'
-    parameters = {'ml': query, 'max': 50, 'sp': '???? - ????????????', 'partOfSpeech':'n'}
-    response = requests.get(url, params= parameters)
+    url = 'https://api.datamuse.com'
+    #parameters = {'ml': query, 'max': 50, 'sp': '???? - ????????????', 'partOfSpeech':'n'}
+    response = requests.get(f'{url}/words?ml={query}&sp>???&max=20')
     return response.json() #ask about this
     
 #getting related words from a theme
 def get_related_words():
     #chooses a random theme from a created list
-    themes = ['music', 'literature', 'weather', 'school', 'technology', 'city', 'farm', 'shopping', 'biomes', 'magazine names', 'beach', 'puzzle games', 'party goods', 'sports']
+    themes = ['music', 'literature', 'weather', 'school', 'technology', 'city', 'farm', 'shopping', 'biomes', 'beach', 'puzzle+games', 'party+goods', 'sports']
     random_theme = random.choice(themes)
     print(random_theme)
     
     #uses the api to find related words and writes to a new textfile
-    related_words = datamuse_api_get(random_theme)
-    if related_words:
-        file_path = f'{random_theme}.txt'
-        #file_path = os.path.join(f'{random_theme}.txt')
-        with open(file_path, 'w') as file:
-            for word in related_words:
-                file.write(word + '\n')
+    retrieved = datamuse_api_get(random_theme)
+    print(retrieved)
+    #prints out retrieved - a list of dictionaries
+    
+    #extract values of the 'word' key in each dictionary
+    #put them into list of strings
+    related_words_prot = [dic['word'] for dic in retrieved]
+    print(related_words_prot)
+
+    #limit word length and omit non alphabetic symbols
+    related_words_fin = [term for term in related_words_prot if len(term) <= 8 and term.isalpha()]
+    print(related_words_fin)
+
+    #creating the textfile
+    file_path = f'{random_theme}.txt'
+    #file_path = os.path.join('finalproject_strands', f'{random_theme}.txt')
+    with open(file_path, 'w') as file:
+        for string in related_words_fin:
+            file.write(string + '\n')
+    print("Text file created successfully:", file_path)  # Print the path of the created file
+
+get_related_words()
+
+
 
 
 
